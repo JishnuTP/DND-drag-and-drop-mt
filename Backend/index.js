cb59c67ddc6f6e644c355ae9230a2998f41c0128
JishnuTP <tpjishnu5@gmail.com>
2025-01-23 18:58:38 +0530

    h

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,20 @@ const dotenv= require("dotenv")
 const MongoDB = require("./config/db");
 const authRoute = require("./routes/AuthRoute");
 const taskRoute = require("./routes/TaskRoute");
-
+const bodyparser =require("body-parser")
+const compression = require("compression")
 dotenv.config()
 
+app.use(bodyparser.json())
 app.use(express.json());
 app.use(cors())
+app.use(compression())
+
+app.use(cors({
+    origin: "https://task-menucard-frontend.vercel.app", // Allow only this origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
+    credentials: true // Allow credentials if needed
+  }));
 
 app.use("/api/auth",authRoute)
 app.use("/api",taskRoute)
